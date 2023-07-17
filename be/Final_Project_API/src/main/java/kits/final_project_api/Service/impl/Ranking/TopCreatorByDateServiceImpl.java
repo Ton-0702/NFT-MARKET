5d78e7044e0e561cf0c3143877e5b0e6d6c220cf
unknown <19447201.iuh@gmail.com>
2023-07-18 00:08:39 +0700

    update Ranking

--- be/Final_Project_API/src/main/java/kits/final_project_api/Service/impl/Ranking/TopCreatorByDateServiceImpl.java
+++ be/Final_Project_API/src/main/java/kits/final_project_api/Service/impl/Ranking/TopCreatorByDateServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -17,15 +16,28 @@ public class TopCreatorByDateServiceImpl implements TopCreatorByDateService {
     private TopCreatorByDateRepository topCreatorByDateRepository;
 
     @Override
-    public List<Map<String, Object>> getTopCreatorToday(Integer page, Integer numberCreator) {
+    public List<Map<String, Object>> getTopCreatorToday(Integer offset, Integer limit) {
         LocalDateTime date_current = LocalDateTime.now();
         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String date_current_format = date_current.format(myFormatObj);
 
 //        Comparator<Map<String, Object>> c = creatorToday.sort(Comparator.comparing((Map<String, Object> m) -> (int) m.get("nft_sold"), Comparator.nullsLast(Comparator.reverseOrder())).thenComparing(m -> (Double) m.get("volume")));
 
+//        Pag page = (Pageable) PageRequest.of(pageable,2);
 
-        return topCreatorByDateRepository.getTopCreatorToday(date_current_format, page, numberCreator);
+//        Pageable page = PageRequest.of(pageable, 2);
+//
+//        Page<Map<String, Object>> pageResult = (Page<Map<String, Object>>) topCreatorByDateRepository.getTopCreatorToday(date_current_format, page);
+
+        return topCreatorByDateRepository.getTopCreatorToday(date_current_format, offset, limit);
+    }
+
+    @Override
+    public List<Map<String, Object>> getTopCreatorAllTime(Integer offset, Integer limit) {
+        LocalDateTime date_current = LocalDateTime.now().minusDays(1);
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String date_current_format = date_current.format(myFormatObj);
+        return topCreatorByDateRepository.getTopCreatorAllTime(date_current_format, offset, limit);
     }
 
 }
